55e521674cb56242ecb7a4e11a82d7ed5def63ef
Shreyash7478 <shturkar90@gmail.com>
2019-12-21 13:39:22 +0530

    added comment for functions

--- circularlinkedlist.h
+++ circularlinkedlist.h
@@ -23,3 +23,19 @@ class CircularLinkedList{
 };
 
 #endif //CIRCULARLINKEDLIST_H
+
+
+/*
+Cicular Linked List
+
+isEmpty() return bool True if empty and false if not empty
+
+insertAtStart(int) takes integer as parameter and adds at start Time complexity : O(1)
+void insertAtEnd(int) takes integer as parameter and adds at end Time complexity : O(1)
+
+deleteAtStart() deletes node at start Time Complexity : O(1)
+
+front() returns integer value of node stored at start O(1)
+
+void display() used for debugging shows entire queue O(n)
+*/
